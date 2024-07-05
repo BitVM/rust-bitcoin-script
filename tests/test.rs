043d63b714874d534d1c050a580b79a889d7158d
Lukas <lucidLuckylee@users.noreply.github.com>
2024-07-05 21:06:40 +0200

    Run first test with chunker

--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,6 @@
 use bitcoin::opcodes::all::OP_ADD;
 use bitcoin_script::{define_pushable, script};
+use pushable::Chunker;
 
 //define_pushable!();
 
@@ -13,6 +14,7 @@ pub mod pushable {
     use std::collections::HashMap;
     use std::convert::TryFrom;
     use std::hash::{DefaultHasher, Hash, Hasher};
+    use std::ops::Deref;
 
     #[derive(Clone, Debug, Hash)]
     enum Block {
@@ -44,30 +46,36 @@ pub mod pushable {
         }
     }
 
-    pub struct Chunker<'a> {
+    pub struct Chunker {
         // Each chunk has to be in the interval [target_chunk_size - tolerance, target_chunk_size]
         target_chunk_size: usize,
         tolerance: usize,
 
-        // Original Builder (Maybe remove this and instead push it as first entry to call_stack
-        // with 0)
-        builder: &'a Builder,
-
-        chunks: Vec<usize>,
+        pub chunks: Vec<usize>,
 
         // Builder Callstack (current builder and where we chunked it; always pos the call that
         // will be chunked + 1)
-        call_stack: Vec<(&'a Builder, usize)>,
+        call_stack: Vec<(Box<Builder>, usize)>,
     }
 
-    impl<'a> Chunker<'a> {
-        fn highest_chunkable_builder(&mut self) -> Result<(&'a Builder, usize), ChunkerError> {
+    impl Chunker {
+        pub fn from_builder(builder: Builder, target_chunk_size: usize, tolerance: usize) -> Self {
+            Chunker {
+                target_chunk_size,
+                tolerance,
+                chunks: vec![],
+                call_stack: vec![(Box::new(builder), 0)],
+            }
+        }
+
+        fn highest_chunkable_builder(&mut self) -> Result<(Box<Builder>, usize), ChunkerError> {
             let mut chunk_size = 0;
-            let result_builder = Err(ChunkerError);
+            let mut result_builder = Err(ChunkerError);
             while chunk_size < self.target_chunk_size {
-                let result_builder = self.call_stack.pop();
-                if let Some((builder, chunk_pos)) = result_builder {
+                let builder_info = self.call_stack.pop();
+                if let Some((builder, chunk_pos)) = builder_info {
                     chunk_size += builder.size - chunk_pos;
+                    result_builder = Ok((builder, chunk_pos));
                 }
             }
             result_builder
@@ -76,18 +84,19 @@ pub mod pushable {
         // Tries to chunk the provided builder starting from start_pos (so every block before
         // start_pos is considered as part of a previous chunk and ignored).
         // Returns ChunkerError if there is no Call block that chunks or overlaps this chunk.
-        // Returns None and appends self.chunks if the builder was chunked.
-        // Otherwise, returns the id of the overlapping Call.
-        // starting_size is the starting size of the chunk (the previous builder could have been
+        // Returns Ok(None) if the builder was chunked.
+        // Otherwise, returns the id of the overlapping Call and the size of the builder until the
+        // overlapping_call
+        // start_size is the starting size of the chunk (the previous builder could have been
         // too small)
         fn try_chunk(
             &mut self,
             builder: &Builder,
-            starting_size: usize,
+            start_size: usize,
             start_pos: usize, // If we chunked the same builder before we have to start
-                                         // after that block and not at the beginning of it
-        ) -> Result<Option<u64>, ChunkerError> {
-            let mut chunk_size = starting_size;
+                              // after that block and not at the beginning of it
+        ) -> Result<Option<(u64, usize)>, ChunkerError> {
+            let mut chunk_size = start_size;
             let mut current_pos = 0;
             let mut overlapping_call = None;
             for block in builder.blocks.iter() {
@@ -97,25 +106,39 @@ pub mod pushable {
                             .script_map
                             .get(id)
                             .expect("Missing entry for a called script");
-                        overlapping_call = Some(*id);
+                        // current_pos is the size of the builder before the overlapping_call
+                        overlapping_call = Some((*id, current_pos));
                         called_script.len()
                     }
                     Block::Script(script) => script.len(),
                 };
                 current_pos += block_len;
+                println!("[INFO] current pos: {:?} - start_pos: {:?}", current_pos, start_pos);
                 // The block is already in the previous chunk
+                // (possibly as an overlapping_call but its remaining size is already accounted for
+                // with start_size)
                 if current_pos < start_pos {
                     continue;
                 }
                 let block_end = block_len + chunk_size;
-                if (block_end < self.target_chunk_size)
-                    && (block_end > self.target_chunk_size - self.tolerance)
+                println!("[INFO] block_end: {:?}", block_end);
+                if (block_end <= self.target_chunk_size + start_size)
+                    && (block_end >= self.target_chunk_size - self.tolerance + start_size)
                 {
+                    println!("[INFO] block_end VALID! block_len: {:?}", block_len);
                     overlapping_call = None;
                     chunk_size += block_len;
+                    println!("[INFO] chunk_size: {:?}", chunk_size);
+                    //TODO we could find a better chunk after the next call if both of them end
+                    //in target_chunk_size - tolerance
+                    //Watch out for the above overlapping_call = Some(*) then because it overrides
+                    //that we found a non-overlapping chunk option
+                    break;
                 }
             }
-            if chunk_size == starting_size {
+
+            println!("[INFO] overlapping_call: {:?}", overlapping_call);
+            if chunk_size == start_size {
                 Err(ChunkerError)
             } else if overlapping_call.is_none() {
                 self.chunks
@@ -126,33 +149,53 @@ pub mod pushable {
             }
         }
 
-        // TODO: Handle the last chunk because it is allowed to be smaller (probably in find_chunks
+        // TODO: Handle the last chunk because it is allowed to be smaller (probably in a find_chunks
         // method)
-        // TODO: Push to the call_stack (Finding out the chunk_pos seems to be an issue)
         pub fn find_next_chunk(&mut self) -> Result<(), ChunkerError> {
-            //// Find the highest still chunkable builder (call_stack.pop_back())
-            // pop call_stack builders until we are over the target_chunk_size
-            let (mut builder, start_pos) = self.highest_chunkable_builder()?;
-            // Try to chunk the builder
-            let mut chunk_result = self.try_chunk(builder, *self.chunks.last().unwrap_or(&0_usize), start_pos);
-            while match chunk_result {
-                Ok(overlapping_call) => match overlapping_call {
-                    Some(_) => {
-                        true
-                    }
-                    None => return Ok(()),
-                },
-                Err(e) => return Err(e),
-            } {
-                // Chunk the overlapping call
-                let overlapping_call = chunk_result.unwrap_or_else(|_| unreachable!()).unwrap_or_else(|| unreachable!());
-                builder = builder
-                    .script_map
-                    .get(&overlapping_call)
-                    .expect("Missing entry for a called script");
-                chunk_result = self.try_chunk(builder, *self.chunks.last().unwrap_or(&0_usize), 0);
+            // Find the highest still chunkable builder on the call_stack
+            let builder_info = self.highest_chunkable_builder()?;
+            println!("[INFO] highest_chunkable builder: {:?}", builder_info);
+            let (mut builder, mut start_pos) = (builder_info.0.deref(), builder_info.1);
+            let mut chunk_result;
+            let mut start_size = start_pos - *self.chunks.last().unwrap_or(&0_usize);
+            loop {
+                // Try to chunk the current builder
+                chunk_result = self.try_chunk(builder, start_size, start_pos);
+                println!("[INFO] chunk_result: {:?}", chunk_result);
+                // As long as the builder has an overlapping_call set builder to the
+                // overlapping_call builder and loop again
+                builder = match chunk_result {
+                    Ok(option) => match option {
+                        Some((call, pos)) => {
+                            start_pos = 0;
+                            start_size = pos;
+                            let next_builder = builder
+                                .script_map
+                                .get(&call)
+                                .expect("Missing entry for a called script");
+                            // Push the builder to call_stack because we are now going a builder
+                            // deeper to chunk it
+                            // Push start_size + next_builder.size as the position where it will be
+                            // chunked (we overshoot this because in this builder we will not go into the overlapping call again to chunk it)
+                            self.call_stack
+                                .push((Box::new(builder.clone()), start_size + next_builder.size));
+                            next_builder
+                        }
+                        None => {
+                            // Check if this builder was chunked at the end
+                            // This is the result of the try_chunk operation
+                            let found_chunk_pos =
+                                *self.chunks.last().unwrap_or_else(|| unreachable!());
+                            if found_chunk_pos - start_size <= builder.size {
+                                self.call_stack
+                                    .push((Box::new(builder.clone()), found_chunk_pos));
+                            }
+                            return Ok(());
+                        }
+                    },
+                    Err(error) => return Err(error),
+                };
             }
-            Ok(())
         }
 
         //pub fn find_chunks(self, tolerance: usize, target_chunk_size: usize) -> Vec<usize> {
@@ -668,4 +711,39 @@ fn test_non_optimal_opcodes() {
         script.compile().as_bytes(),
         vec![124, 109, 122, 124, 123, 83, 124, 123, 83, 122]
     );
+}
+
+#[test]
+fn test_chunker_simple() {
+    let sub_script = script! {
+        OP_ADD
+        OP_ADD
+    };
+
+    let script = script! {
+        { sub_script.clone() }
+        { sub_script.clone() }
+        { sub_script.clone() }
+        { sub_script.clone() }
+    };
+
+    println!("{:?}", script);
+
+    let mut chunker = Chunker::from_builder(script, 2, 0);
+    chunker
+        .find_next_chunk()
+        .expect("Failed to find first chunk");
+    println!("[INFO] chunk positions after first_chunk: {:?}", chunker.chunks);
+    chunker
+        .find_next_chunk()
+        .expect("Failed to find second chunk");
+    println!("[INFO] chunk positions after second chunk: {:?}", chunker.chunks);
+    chunker
+        .find_next_chunk()
+        .expect("Failed to find second chunk");
+    println!("[INFO] chunk positions after third chunk: {:?}", chunker.chunks);
+    chunker
+        .find_next_chunk()
+        .expect("Failed to find second chunk");
+    println!("[INFO] chunk positions after fourth chunk: {:?}", chunker.chunks);
 }
