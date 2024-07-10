ee280012ae35d28ce99cde92630907d54241ef84
Lukas <lucidLuckylee@users.noreply.github.com>
2024-07-10 12:02:29 +0200

    Seperate Macro into proc_macro and treepp crate

---
+++ macro/src/lib.rs
@@ -0,0 +1,15 @@
+mod generate;
+mod parse;
+
+use generate::generate;
+use parse::parse;
+use proc_macro::TokenStream;
+use proc_macro_error::{proc_macro_error, set_dummy};
+use quote::quote;
+
+#[proc_macro]
+#[proc_macro_error]
+pub fn script(tokens: TokenStream) -> TokenStream {
+    set_dummy(quote!((::bitcoin::Script::new())));
+    generate(parse(tokens.into())).into()
+}
