b05d515d3dbb6a282f83f2fb99d4c12620b1ae4c
Lukas <lucidLuckylee@users.noreply.github.com>
2024-07-27 23:22:11 +0200

    cleanup

--- src/chunker.rs
+++ src/chunker.rs
@@ -3,14 +3,34 @@ use crate::{
     builder::{Block, Builder},
     StackAnalyzer,
 };
-use core::fmt;
+
 
 #[derive(Debug, Clone)]
-pub struct ChunkerError;
+struct ChunkStats {
+    stack_input_size: usize,
+    stack_output_size: usize,
+    altstack_input_size: usize,
+    altstack_output_size: usize,
+}
 
-impl fmt::Display for ChunkerError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Unable to chunk with set parameters")
+#[derive(Debug, Clone)]
+pub struct Chunk {
+    scripts: Vec<Box<Builder>>,
+    size: usize,
+    stats: Option<ChunkStats>,
+}
+
+impl Chunk {
+    pub fn new(scripts: Vec<Box<Builder>>, size: usize) -> Chunk {
+        Chunk {
+            scripts,
+            size,
+            stats: None,
+        }
+    }
+
+    pub fn scripts(self) -> Vec<Box<Builder>>{
+        self.scripts
     }
 }
 
@@ -20,7 +40,7 @@ pub struct Chunker {
     tolerance: usize,
 
     size: usize,
-    pub chunks: Vec<Vec<Box<Builder>>>,
+    pub chunks: Vec<Chunk>,
 
     // Builder Callstack (consists of remaining structured scripts)
     pub call_stack: Vec<Box<Builder>>,
@@ -37,33 +57,30 @@ impl Chunker {
         }
     }
 
-    pub fn find_chunks_and_analyze_stack(&mut self) -> Vec<(usize, usize, usize, usize, usize)> {
+    pub fn find_chunks_and_analyze_stack(&mut self) -> Vec<Chunk> {
         let mut chunks = vec![];
         while !self.call_stack.is_empty() {
             let chunk = self.find_next_chunk();
             chunks.push(chunk);
         }
-        let mut result = vec![];
         for chunk in chunks.iter_mut() {
-            let chunk_size = chunk.1;
             // println!("chunk size: {}", chunk_size);
-            let status = self.stack_analyze(&mut chunk.0);
+            let status = self.stack_analyze(&mut chunk.scripts);
             // println!("stack_analyze: {:?}", status);
             // ((-1 * access) as u32, (depth - access) as u32)
-            let stack_input_size = (-1 * status.deepest_stack_accessed) as usize;
+            let stack_input_size = status.deepest_stack_accessed.abs() as usize;
             let stack_output_size = (status.stack_changed - status.deepest_stack_accessed) as usize;
-            let altstack_input_size = (-1 * status.deepest_altstack_accessed) as usize;
+            let altstack_input_size = status.deepest_altstack_accessed.abs() as usize;
             let altstack_output_size =
                 (status.altstack_changed - status.deepest_altstack_accessed) as usize;
-            result.push((
-                chunk_size,
+            chunk.stats = Some(ChunkStats {
                 stack_input_size,
                 stack_output_size,
                 altstack_input_size,
                 altstack_output_size,
-            ));
+            });
         }
-        result
+        chunks
     }
 
     fn stack_analyze(&self, chunk: &mut Vec<Box<Builder>>) -> StackStatus {
@@ -71,7 +88,7 @@ impl Chunker {
         stack_analyzer.analyze_blocks(chunk)
     }
 
-    fn find_next_chunk(&mut self) -> (Vec<Box<Builder>>, usize) {
+    fn find_next_chunk(&mut self) -> Chunk {
         let mut result = vec![];
         let mut result_len = 0;
         loop {
@@ -79,6 +96,10 @@ impl Chunker {
                 Some(builder) => *builder,
                 None => break, // the last block in the call stack
             };
+
+            // TODO: Use stack analysis to find best possible chunk border
+            // TODO: Consider chunks that are closest to the tolerance first (e.g. if target = 300
+            // and tolerance = 20 we can split at 299 and 290 but 299 should be preferred.
             let block_len = builder.len();
             if result_len + block_len < self.target_chunk_size - self.tolerance {
                 result.push(Box::new(builder));
@@ -110,15 +131,15 @@ impl Chunker {
                 break;
             }
         }
-        (result, result_len)
+        Chunk::new(result, result_len)
     }
 
     pub fn find_chunks(&mut self) -> Vec<usize> {
         let mut result = vec![];
         while !self.call_stack.is_empty() {
-            let (chunk, size) = self.find_next_chunk();
+            let chunk = self.find_next_chunk();
+            result.push(chunk.size);
             self.chunks.push(chunk);
-            result.push(size);
         }
         result
     }
--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,5 @@ pub mod chunker;
 
 pub use crate::builder::Builder as Script;
 pub use analyzer::StackAnalyzer;
-pub use chunker::{Chunker, ChunkerError};
+pub use chunker::Chunker;
 pub use script_macro::script;
